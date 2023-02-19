f96a2a8bbdade69684f7a966d2dbeac5409fdcb8
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-19 15:36:39 +0330

    add: subscribe example

---
+++ example/subscribe/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	sdk "github.com/official-stallion/go-sdk"
+)
+
+func main() {
+	client, err := sdk.NewClient("st://localhost:9090")
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 2; i++ {
+		go func(j int) {
+			cli, er := sdk.NewClient("st://localhost:9090")
+			if er != nil {
+				log.Fatal(er)
+			}
+
+			cli.Subscribe("topic1", func(bytes []byte) {
+				log.Printf("%d: %s\n", j, string(bytes))
+			})
+
+			cli.Subscribe("topic2", func(bytes []byte) {
+				log.Printf("%d second: %s\n", j, string(bytes))
+			})
+
+			select {}
+		}(i)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	client.Publish("topic1", []byte("Hello from 1"))
+	client.Publish("topic2", []byte("Hello from 2"))
+
+	select {}
+}
